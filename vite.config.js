7b19cc6d35257a2a261f0ebcaf8ac4ab779e9c79
kgmoneyitb <117512332+kwstahl@users.noreply.github.com>
2026-03-07 19:40:51 -0600

    reduced images

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      mozjpeg: {
+        quality: 70,
+      },
+      pngquant: {
+        quality: [0.6, 0.8],
+      },
+      webp: {
+        quality: 75,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "figma:asset": path.resolve(__dirname, "src/assets/figma"),
