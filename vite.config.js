5a6224a46e644fd2e6e7b05871953e5c3b5f4958
kgmoneyitb <117512332+kwstahl@users.noreply.github.com>
2026-03-08 01:40:18 -0600

    fixed quality of hero removed vite config plugin

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      mozjpeg: {
-        quality: 70,
-      },
-      pngquant: {
-        quality: [0.6, 0.8],
-      },
-      webp: {
-        quality: 75,
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "figma:asset": path.resolve(__dirname, "src/assets/figma"),
